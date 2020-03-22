71148e47fbede9d81fb2fb3e126e0673c51396a0
Francesco Crisanti <fcrisanti@gmail.com>
2020-03-22 10:15:48 +0100

    Added Spring Security, to log in use admin/admin, user/user. Now only Admin can add projects and employees. Switched to H2 for testing.

---
+++ src/main/java/pl/fc/app/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package pl.fc.app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+// For PostgreSQL:        .usersByUsernameQuery(
+//                        "select username,password, enabled from users where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, role from user_roles where username=?")
+
+// For H2:
+                .withDefaultSchema()
+                .withUser("admin")
+                .roles("ADMIN")
+                .password("admin")
+                .and()
+                .withUser("user")
+                .password("user")
+                .roles("USER");
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/h2_console/**").permitAll()
+                .antMatchers("/").authenticated().and().formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
