453f649fe7271fe60dc71020082055bdb58bba92
Francesco Crisanti <fcrisanti@gmail.com>
2020-05-20 09:13:25 +0200

    Disabled authorisation for demo purpose

--- src/main/java/pl/fc/app/security/WebConfig.java
+++ src/main/java/pl/fc/app/security/WebConfig.java
@@ -15,15 +15,4 @@ class WebConfig implements WebMvcConfigurer {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
-
 }
