3e6e1497e9b1973cd78e6bf7bf3f8550ec1b7a53
Francesco Crisanti <fcrisanti@gmail.com>
2020-08-29 11:09:16 +0200

    RWD adjustment for smaller screens

--- src/main/java/pl/fc/app/security/PermissionManager.java
+++ src/main/java/pl/fc/app/security/PermissionManager.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.fc.app.dao.IUserRepository;
-import pl.fc.app.enities.UserAccount;
 
 @Service
 public class PermissionManager {
